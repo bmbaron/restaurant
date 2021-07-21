d87f6beeb97882b6860f32503cc04b27c213f807
Ben Baron <benjamin.m.baron@gmail.com>
2021-07-21 11:34:31 +0700

    made the home and contact pages more responsive

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,6 +5,8 @@ const contactPage = (() => {
   
   const map = document.createElement('iframe');
   map.src="https://www.google.com/maps/embed/v1/view?zoom=17&center=41.6555%2C-91.5347&key=AIzaSyCqjYtvaGxWMiJu5lOCxMoSkyWaoDTnbvA";
+  map.title = 'some title';
+  map.frameBorder = 0;
   
   const description = document.createElement('div');
   const title = document.createElement('h2');
@@ -12,21 +14,23 @@ const contactPage = (() => {
   description.appendChild(title);
   
   const waysToContact = document.createElement('ul');
+  
+    const iconContainer =document.createElement('div');
     const numIcon = document.createElement('mat-icon');
       numIcon.className = "material-icons";
       numIcon.classList.add("mat-icon")
       numIcon.innerHTML = "&#xe0b0";
-      description.appendChild(numIcon);
+      iconContainer.appendChild(numIcon);
     const mailIcon = document.createElement('mat-icon');
       mailIcon.className = "material-icons";
       mailIcon.classList.add("mat-icon");
       mailIcon.innerHTML = "&#xe0be";
-      description.appendChild(mailIcon);
+      iconContainer.appendChild(mailIcon);
     const fbIcon = document.createElement('mat-icon');
       fbIcon.className = "material-icons";
       fbIcon.classList.add("mat-icon");
       fbIcon.innerHTML = "&#xf234";
-      description.appendChild(fbIcon);
+      iconContainer.appendChild(fbIcon);
   
   const number = document.createElement('li');
   number.innerHTML = "0987654321"
@@ -36,13 +40,13 @@ const contactPage = (() => {
   const facebook = document.createElement('li');
     facebook.innerHTML = "fb.com/tasteofvietnamIC";
   
-  waysToContact.append(number, email, facebook);
+  waysToContact.append(iconContainer, number, email, facebook);
 
 
   description.appendChild(waysToContact);
   
   const createContactPage = () =>
-    document.getElementById('content').append(headline, map, description);
+    document.getElementById('content').append(headline, description, map);
   
     
   return {
